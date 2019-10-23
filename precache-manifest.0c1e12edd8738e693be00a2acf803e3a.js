a7c2e092b5724683c7eab8f0fc5b3aa409e86332
Gustavo Carvalho <gstcarv@gmail.com>
2019-10-23 13:29:02 -0300

    Build

---
+++ precache-manifest.0c1e12edd8738e693be00a2acf803e3a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1c65c100cfb2bb8511fd0ca9f17f6a8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bc5804970349d1f23e35",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "b7a64c6ee86797890d12",
+    "url": "/static/css/main.7b14b430.chunk.css"
+  },
+  {
+    "revision": "bc5804970349d1f23e35",
+    "url": "/static/js/2.f6471583.chunk.js"
+  },
+  {
+    "revision": "b7a64c6ee86797890d12",
+    "url": "/static/js/main.2c937f1e.chunk.js"
+  },
+  {
+    "revision": "a4203712e637cade0150",
+    "url": "/static/js/runtime-main.d3222acf.js"
+  }
+]);
--- src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './pages'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import '@fortawesome/fontawesome-svg-core'
-
-ReactDOM.render(<App />, document.getElementById('root'));
